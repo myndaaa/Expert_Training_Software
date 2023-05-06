632f1690a366e40aa40c45dbefef5ddbc8c17bb8
Ntemesha Ntambu <109055758+ntemesha@users.noreply.github.com>
2023-05-06 22:09:35 +0800

    payment in progress

---
+++ Ntemesha/processingPayment.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styles/styles.css" />
+    <title>Document</title>
+</head>
+<nav>
+    <ul>
+        <li class="">
+            <a class="" href="#">
+                <img src="images/icons8-questions-48.png" alt="Icon" width="30" height="30" class="" />
+                Request
+            </a>
+        </li>
+        <li class="">
+            <a class="" href="#">
+                <img src="images/icons8-idea-48 (1).png" alt="Icon" width="30" height="30" class="" />
+                Suggestion
+            </a>
+        </li>
+        <li class="">
+            <a class="" href="#">
+                <img src="images/icons8-iphone-spinner-48.png" alt="Icon" width="30" height="30" class="" />
+                Processing
+            </a>
+        </li>
+        <li class="">
+            <a class="" href="#">
+                <img src="images/icons8-online-payment-48.png" alt="Icon" width="30" height="30" class="" />
+                Payment
+            </a>
+        </li>
+        <li class="">
+            <a class="" href="#">
+                <img src="images/icons8-chat-message-96 (1).png" alt="Icon" width="30" height="30" class="" />
+                Communication
+            </a>
+        </li>
+    </ul>
+</nav>
+
+<body>
+    <div class="containerProcessing">
+        <img src="https://media.giphy.com/media/grNkIEN4dkiMXFLIE9/giphy.gif" alt="Loading Icon" class="icon" />
+        <div class="title-container">
+            <h1 class="title">Payment in Progress</h1>
+        </div>
+        <p class="message">
+            Please check the <a href="Payment_status_page.php" class="link">Payment status page</a> for confirmation that your payment has been successful.
+        </p>
+    </div>
+</body>
+
+</html>
